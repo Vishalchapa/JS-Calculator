102dacbc855d0bf2df505c80d659ff6194661e4e
Vishal Chandra <chapavishal3@gmail.com>
2024-08-17 14:02:50 +0000

    Generating Random Numbers

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,12 +12,17 @@ document.addEventListener("DOMContentLoaded", function() {
                 let gameType = this.getAttribute("data-type");
                 alert(`You clicked ${gameType}`);
             }
-        })
+        });
     }
-})
+});
+
+// The main game "loop", called when the script is first loaded and after the user's answer has been processed
 
 function runGame() {
 
+    // Creates two random numbers between 1 and 25
+    let num1 = Math.floor(Math.random() * 25) + 1;
+    let num2 = Math.floor(Math.random() * 25) + 1;
 }
 
 function checkAnswer() {
@@ -36,7 +41,7 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(operand1, operand2) {
 
 }
 
