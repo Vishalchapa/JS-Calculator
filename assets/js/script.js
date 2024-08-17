e54515f1aa95f6c6babc889b9b8601df72e4112d
Vishal Chandra <chapavishal3@gmail.com>
2024-08-17 14:52:56 +0000

    Calculating The Correct Answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,6 +39,21 @@ function checkAnswer() {
 
 }
 
+// Gets the operands 
+
+function calculateCorrectAnswer() {
+    let operand1 = parseInt(document.getElementById('operand1').innerText);
+    let operand2 = parseInt(document.getElementById('operand2').innerText);
+    let operator = document.getElementById("operator").innerText;
+
+    if (operator === "+") {
+        return [operand1 + operand2, "addition"];
+    } else {
+        alert(`Unimplemented operator ${operator}`);
+        throw `Unimplemented operator ${operator}. Aborting!`;
+    }
+}
+
 function calculateCorrectAnswer() {
 
 }
@@ -54,7 +69,7 @@ function incrementWrongAnswer() {
 function displayAdditionQuestion(operand1, operand2) {
     document.getElementById('operand1').textContent = operand1;
     document.getElementById('operand2').textContent = operand2;
-    document.getElementById('operator').textContent = "+"
+    document.getElementById('operator').textContent = "+";
 
 }
 
